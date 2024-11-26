f6a6188a2eb3ecaa9efba3117d91a87c6245901c
HISMWalls <mwallace@hearnindustrial.com>
2024-11-25 22:55:35 -0500

    Updated.

--- Enable_NewsNInt_v2.5.1.ps1
+++ Enable_NewsNInt_v2.5.1.ps1
@@ -1,28 +1,24 @@
 $os = [System.Environment]::OSVersion.Version
 
-function Disable-NewsAndInterests {
+function Enable-NewsAndInterests {
     $isWindows10 = $os -match "9."
     $regPath10 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds"
     $regName10 = "EnableFeeds"
     $regPath11 = "HKLM:\SOFTWARE\Policies\Microsoft\Dsh"
     $regName11 = "AllowNewsAndInterests"
 
-    Write-Host "Disabling News and Interests"
-    if (!(Test-Path $regPath10)) {
-        New-Item -Path $regPath10 -Force | Out-Null
-    }
-    Set-ItemProperty -Path $regPath10 -Name $regName10 -Value 0 -Type DWord
+    Write-Host "Enabling News and Interests"
+    Remove-ItemProperty -Path $regPath10 -Name $regName10 -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path $regPath10 -Force -ErrorAction SilentlyContinue
 
     if (-not $isWindows10) {
-        if (!(Test-Path $regPath11)) {
-            New-Item -Path $regPath11 -Force | Out-Null
-        }
-        Set-ItemProperty -Path $regPath10 -Name $regName10 -Value 0 -Type DWord
-        Set-ItemProperty -Path $regPath11 -Name $regName11 -Value 0 -Type DWord
+        Remove-ItemProperty -Path $regPath11 -Name $regName11 -Force -ErrorAction SilentlyContinue
+        Remove-ItemProperty -Path $regPath10 -Name $regName10 -Force -ErrorAction SilentlyContinue
+        Remove-Item -Path $regPath11 -Force -ErrorAction SilentlyContinue
+        Remove-Item -Path $regPath10 -Force -ErrorAction SilentlyContinue
     }
-
 }
 
-Disable-NewsAndInterests
+Enable-NewsAndInterests
 
 shutdown.exe -r -t 00
